eb4cc2c60bc5ca1553373cfa69984b7b08055c3f
Goncalves95 <>
2023-09-24 16:30:04 +0000

    Fixing a problem on area game, dont appear the black backgroud

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-window.onload = function () {
+window.onload = function() {
 
     // Pick up on element from canvas by ID
     let area = document.getElementById('area');
+    //Let the area contxt to 2D
+    let contx = area.getContext("2d");
 
     // Score Board
     let poits = document.getElementById('poits');
     let poiti = 0;
 
-    //Let the area contxt to 2D
-    let contx = area.getContext("2d");
 
     document.addEventListener("keydown", keyPush);
 
-    const easy = document.getElementById('easy');
-    const medium = document.getElementById('medium');
-    const hard = document.getElementById('hard');
+    const easy = document.getElementById('js-easy');
+    const medium = document.getElementById('js-medium');
+    const hard = document.getElementById('js-hard');
 
     setInterval(game, 70);
 
@@ -49,46 +49,47 @@ window.onload = function () {
 
         // For snake control on the boards rps
         if (stx < 0) {
-            stx = qtp-1;
-        }
-        if (stx > qtp-1) {
-            stx = 0;
-        }
-        if (sty < 0) {
             sty = qtp-1;
         }
-        if (py > qtp-1) {
+        if (stx > qtp-1) {
+            sty = 0;
+        }
+        if (stx < 0) {
+            sty = qtp-1;
+        }
+        if (stx > qtp-1) {
             sty = 0;
         }
 
-        // javascript contex.fillRect (x, y, = altura, largura)
-        contx.fillStyle = "black";
-        contx.fillRect(0,0, area.width, area.height);
         
-        contx.fillStyle = "red"
+        contx.fillStyle ="#111D4A";
+        // javascript contex.fillRect (x, y, = altura, largura)
+        contx.fillRect(0, 0, area.width, area.height);
+        
+        contx.fillStyle = "#EA2B1F";
         contx.fillRect(mousex * psz, mousey * psz, psz, psz, 2, 2);
 
         // For ([inicialExpretion]; [condition]; [increment]) declaration
         for (let i = 0; i < tail.length; i++) {
-            contx.fillStyle = "green"
-            contx.stokeStyle = "blue"
+            contx.fillStyle ="green"
+            contx.stokeStyle ="green"
             contx.fillRect(tail[i].x * psz, tail[i].y * psz, psz, psz);
             contx.strokeRect(tail[i].x * psz, tail[i].y * psz, psz, psz);
             if (tail[i].x == stx && tail[i].y == sty) {
                 spx = spy = 0;
-                tails = 2;
+                taile = 2;
                 poiti = 0;
             }
         }
 
         tail.push({x: stx, y: sty})
 
-        while (tail.length > tails) {
+        while (tail.length > taile) {
             tail.shift()
         }
 
         if (mousex==stx && mousey==sty){
-            tails++
+            taile++
             mousex = Math.floor(Math.random() * qtp)
             mousey = Math.floor(Math.random() * qtp)
 
